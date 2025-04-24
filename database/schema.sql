3591c4031a4e5760b34ffb010b202a781c0b63b3
Kyle Spence <kyle.w.spence@gmail.com>
2025-04-24 18:43:25 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,55 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "userName" text,
+  "hashedPassword" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "properties" (
+  "propertyId" serial PRIMARY KEY,
+  "address" text,
+  "estValue" integer,
+  "range" integer,
+  "type" text,
+  "beds" text,
+  "bath" text,
+  "sqft" integer,
+  "built" integer,
+  "lastSale" text
+);
+
+CREATE TABLE "transactions" (
+  "transactionId" serial PRIMARY KEY,
+  "date" integer,
+  "description" text,
+  "category" text,
+  "amount" integer
+);
+
+CREATE TABLE "holdings" (
+  "holdingId" serial PRIMARY KEY,
+  "shares" integer,
+  "symbol" text,
+  "sector" text
+);
+
+CREATE TABLE "insights" (
+  "insightsId" serial PRIMARY KEY,
+  "goals" text,
+  "health" integer,
+  "ageGroupCompare" integer,
+  "spendingAlert" text,
+  "savingsRate" text
+);
+
+ALTER TABLE "properties" ADD FOREIGN KEY ("propertyId") REFERENCES "users" ("userId");
+
+ALTER TABLE "transactions" ADD FOREIGN KEY ("transactionId") REFERENCES "users" ("userId");
+
+ALTER TABLE "holdings" ADD FOREIGN KEY ("holdingId") REFERENCES "users" ("userId");
+
+ALTER TABLE "insights" ADD FOREIGN KEY ("insightsId") REFERENCES "users" ("userId");
