2aaa0c95104b3a9cd2a8e48b020d637bed46233b
Kyle Spence <kyle.w.spence@gmail.com>
2025-05-08 19:25:41 +0000

    added insert statement to data.sql

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,10 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+
+INSERT INTO users (id, username, password_hash)
+VALUES
+  (1, 'kdubs1', '$argon2id$v=19$m=65536,t=3,p=4$hJZ+bXoGbVpKLDy/OBtaTg$lpDtMvEx+Im5T5BzwbELTnHFwif8wiEzha6cy5flMLc');
+
+  
